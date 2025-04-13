1f6c494e1a2d1763ea39e31c3265895cdb0fa375
Rezowan6 <mdrezowanmiya6@gmail.com>
2025-04-13 18:00:37 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/*===typing script===*/
+
+const typed_script = new Typed ("#typed_script",{
+    strings: ["Web Developer!", "Web Designer!"],
+    typeSpeed: 80,
+    backSpeed: 70,
+    loop: true,
+})
